746a3a000b5a996f5f04e4f15c52542afb1fb10d
Amit Kumar Khanra <amitkumar.khanra@gmail.com>
2014-03-21 01:10:50 +0530

    Added DB scripts

---
+++ src/main/resources/db-scripts/artzv_tables_insert.sql
@@ -0,0 +1,8 @@
+Insert into ARTZV_ADDR_TYPE (ADDR_TYPE,ADDR_DESCRIPTION) values ('P','Permanent Address');
+Insert into ARTZV_ADDR_TYPE (ADDR_TYPE,ADDR_DESCRIPTION) values ('C','Current Address');
+Insert into ARTZV_ADDR_TYPE (ADDR_TYPE,ADDR_DESCRIPTION) values ('B','Billing Address');
+
+Insert into ARTZV_PHONE_TYPE (PHONE_TYPE,PHONE_DESCRIPTION) values ('W','Work Phone');
+Insert into ARTZV_PHONE_TYPE (PHONE_TYPE,PHONE_DESCRIPTION) values ('P','Personal Phone');
+Insert into ARTZV_PHONE_TYPE (PHONE_TYPE,PHONE_DESCRIPTION) values ('M','Mobile Phone');
+
